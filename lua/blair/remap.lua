fba567c219e7956f7ca40d68278cb3a9684aa96d
geist-2501 <blaironbute@gmail.com>
2025-12-22 12:40:15 +0000

    add border to hover info

--- lua/blair/remap.lua
+++ lua/blair/remap.lua
@@ -2,4 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
-vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "K", function ()
+    vim.lsp.buf.hover({ border = "rounded"})
+end)
