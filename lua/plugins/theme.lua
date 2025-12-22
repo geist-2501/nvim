061522fb4eecbb59ec783fac7f20e959873202d1
geist-2501 <blaironbute@gmail.com>
2025-12-22 12:39:54 +0000

    changed to a theme that supports treesitter

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,8 @@
 return {
-  "nyoom-engineering/oxocarbon.nvim",
-  config = function ()
-      vim.opt.background = "dark"
-      vim.cmd("colorscheme oxocarbon")
-  end
+   "vague-theme/vague.nvim",
+   lazy = false,
+   config = function ()
+       vim.opt.background = "dark"
+       vim.cmd("colorscheme vague")
+   end
 }
