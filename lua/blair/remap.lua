996620c810c68e394e881e5a4e0eebb12057a15a
geist-2501 <blaironbute@gmail.com>
2025-12-29 18:24:02 +0000

    Add conform

--- lua/blair/remap.lua
+++ lua/blair/remap.lua
@@ -5,3 +5,7 @@ vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
 vim.keymap.set("n", "K", function ()
     vim.lsp.buf.hover({ border = "rounded"})
 end)
+
+vim.keymap.set({ "n", "v" }, "<leader>f", function()
+    require("conform").format({ async = true, lsp_fallback = true })
+end, { desc = "Format buffer" })
